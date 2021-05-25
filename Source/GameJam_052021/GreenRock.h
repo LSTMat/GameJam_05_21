8c592a212a649e279c82bb31de9d3b25d6dbde22
Dregon <edoardorimoldi18@gmail.com>
2021-05-26 00:00:57 +0200

    Initialized new classes for items

---
+++ Source/GameJam_052021/GreenRock.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items.h"
+#include "GreenRock.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_052021_API UGreenRock : public UItems
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameJam_052021/Items.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Items.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM_052021_API UItems : public UObject
+{
+	GENERATED_BODY()
+	
+};
